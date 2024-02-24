425b07fc915a29772d8211c00ff5bba4850b8e87
senseiGai <swerss564@gmail.com>
2024-02-24 22:02:31 +0600

    version 1.0.8

--- src/widgets/Main/index.tsx
+++ src/widgets/Main/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+// import { Parallax } from "react-scroll-parallax";
+import { useScroll, useTransform, motion } from "framer-motion";
+import { useRef } from "react";
 
 import Header from "@features/Header";
 
@@ -7,14 +10,28 @@ import styles from "../Main/styles.module.scss";
 import Logo from "@assets/Logo/logo.svg";
 
 function MainScreen() {
+  const main = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: main,
+    offset: ["start start", "end end"],
+  });
+
+  const scale4 = useTransform(scrollYProgress, [0, 1], [2, 5]);
+
   return (
-    <section className={styles.main}>
-      <main className={styles.main__content}>
-        <Header />
-        <div className={styles.main__content_logo}>
-          <img src={Logo} alt="logo" />
-        </div>
-      </main>
+    <section ref={main} className={styles.main}>
+      <Header />
+      <div className={styles.main__sticky}>
+        <main className={styles.main__content}>
+          <motion.div
+            style={{ scale: scale4 }}
+            className={styles.main__content_logo}
+          >
+            <img src={Logo} alt="logo" />
+          </motion.div>
+        </main>
+      </div>
     </section>
   );
 }
